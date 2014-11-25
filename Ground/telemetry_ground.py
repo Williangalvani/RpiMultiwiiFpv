f09c544369a50e167b04d55d20fbbe3c5ae04f85
will <williangalvani@gmail.com>
2014-11-25 12:14:41 +0200

    added soon-to-be telemetry

---
+++ Ground/telemetry_ground.py
@@ -0,0 +1,17 @@
+__author__ = 'will'
+
+
+import socket
+
+# This is an example of a UDP client - it creates
+# a socket and sends data through it
+
+# create the UDP socket
+UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+data = " jasjlsdfjk sdkldfkjlsd jlksfjklsd\n"
+
+# Simply set up a target address and port ...
+addr = ("192.168.42.1",21567)
+# ... and send data out to it!
+UDPSock.sendto(data,addr)
